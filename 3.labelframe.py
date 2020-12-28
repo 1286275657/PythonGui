9281c462f7c68b70c7cda8ca2df7d9d48f830729
wangkai <1286275657@qq.com>
2020-12-28 18:44:20 +0800

    V1.1

--- 3.labelframe.py
+++ 3.labelframe.py
@@ -1,3 +1,16 @@
+'''
+1.导入tkinter组建
+2.实例化Tk()类对象root
+3.在root中 实例化LabelFrame类对象group
+4.显示group  padx=10 pad=10
+5.创建LANGS列表
+6.实例化IntVar类对象v
+7.v.set()方法设置v=1
+8.在group中 实例化Radiobutton类对象b
+9.b.pack   anchor=W   靠西，即使靠左
+10.mainloop()
+'''
+
 from tkinter import *
 
 root = Tk()
@@ -5,3 +18,13 @@ root = Tk()
 group = LabelFrame(root,text="最好的脚本语言是？",padx=5,pady=5)
 group.pack(padx=10,pady=10)
 
+LANGS = [("pthon",1),("perl",2),("ruby",3),("lua",4)]
+
+v=IntVar()
+v.set(1)
+
+for langs,num in LANGS:
+    b = Radiobutton(group,text=langs,variable=v,value=num)
+    b.pack(anchor = E)
+
+mainloop()
